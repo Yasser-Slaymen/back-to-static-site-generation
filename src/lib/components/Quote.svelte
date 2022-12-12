b2c507e52191934e4078d790dc289bda3c9b80b2
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-12-12 21:20:12 +0100

    generating the project

---
+++ src/lib/components/Quote.svelte
@@ -0,0 +1,19 @@
+<section class="quoute-containar">
+    <span class="quote-content">Creatring Tomorrow</span>
+</section>
+
+<style>
+    .quoute-containar{
+        margin-top: 3rem;
+    }
+     .quote-content{
+      display: grid;
+      place-items: left;
+      width: 100vw;
+      margin-left: 3%;
+      font-size: 1.5em;
+      color: var(--white);
+      margin-bottom: 1em;
+    }  
+    
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte'
+	import Hva from '$lib/components/Hva.svelte';
+	import Quote from '$lib/components/Quote.svelte';
+	import './styles.css';
+</script>
+
+<body>
+	<Hva/>
+	<div class="app">
+		<Header/>
+		<main>
+			<slot />
+		</main>
+		<Footer/>
+	</div>
+	<section>	
+			<Quote/>
+	</section>
+	
+	
+</body>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		min-height: 100vh;
+		background-color:var(--green);
+		border-radius: 1em;
+		width: 1080px;
+
+	}
+	
+
+	@media (min-width: 480px) {
+		
+	}
+</style>
